4f900112aca736010179d0813d364d62385a5c47
joxpulp <joxpulp@gmail.com>
2021-10-12 13:32:54 -0500

    fixes

--- server/src/services/server.ts
+++ server/src/services/server.ts
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import session from 'express-session';
 import passport from '../middlewares/facebook-auth'
+import path from 'path'
 import connectMongo from 'connect-mongo';
 import { mongoose } from '../db/mongoose';
 import apiRouter from '../routes/index';
@@ -12,6 +13,7 @@ import apiRouter from '../routes/index';
 mongoose();
 const app = express();
 const server = new http.Server(app);
+app.use(express.static(path.resolve('public')));
 
 app.set('trust proxy', 1);
 app.set('json spaces', 2);
@@ -27,19 +29,20 @@ app.use(
 	session({
 		store: connectMongo.create({ mongoUrl: CONFIG.MONGO_URL }),
 		secret: CONFIG.SECRET,
-		cookie: { sameSite: 'none', secure: 'auto', maxAge: 1000 * 120 },
+		cookie: { secure: 'auto', maxAge: 1000 * 120 },
 		saveUninitialized: false,
 		resave: true,
-		rolling: true
+		rolling: true,
 	})
 );
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.json()); // Indica que el body viene como JSON
 app.use(express.urlencoded({ extended: true })); // Indica que el body puede tener un informacion como no string
-app.get('/', (req: Request, res: Response) => {
-	res.json({ msg: 'Connected to the API' });
-});
 app.use('/api', apiRouter);
+app.get('*', (req: Request, res: Response) => {
+	const indexHtml = path.resolve('../../public/index.html');
+	res.sendFile(indexHtml);
+});
 
 export default server;
