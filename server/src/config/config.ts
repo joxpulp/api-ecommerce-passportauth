a8fc869e386419c4d1e5325c9e421c8ed48427b8
joxpulp <joxpulp@gmail.com>
2021-10-14 22:29:00 -0500

    featL add CLI options and child-process

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -12,16 +12,16 @@ const flags = args.parse(process.argv)
 
 
 export const CONFIG = {
-	PORT: flags.port || 3001,
-	MONGO_URL: process.env.MONGO_URL || 'MONGO-URL',
+	PORT: flags.port || process.env.PORT,
+	MONGO_URL: process.env.MONGO_URL || 'MONGO-URL ',
 	SECRET: process.env.SECRET || 'mysecret',
-	FB_CLIENT: flags.FB_CLIENT_ID || '293339155659928',
-	FB_SECRET: flags.FB_CLIENT_SECRET || 'b1f8348753fa46dc94734555c137af9f',
+	FB_CLIENT: flags.FB_CLIENT_ID || process.env.FB_CLIENT,
+	FB_SECRET: flags.FB_CLIENT_SECRET || process.env.FB_SECRET,
 	FB_CALLBACK_URL:
 		process.env.FB_CALLBACK_URL ||
 		'http://localhost:8080/api/auth/facebook-callback',
 	SUCCESS_REDIRECT: process.env.SUCCESS_REDIRECT || 'http://localhost:3000',
 	FAILURE_REDIRECT:
 		process.env.FAILURE_REDIRECT || 'http://localhost:3000/login',
-	PROCESS_ID: process.pid
+	PROCESS_ID: process.pid,
 };
