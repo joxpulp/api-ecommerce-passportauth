a8987c35f6ec1b3f4cbfea3296dd0fa4437ace4b
joxpulp <joxpulp@gmail.com>
2021-10-14 22:33:46 -0500

    chore: socket.ts updated

--- server/src/services/socket.ts
+++ server/src/services/socket.ts
@@ -7,7 +7,7 @@ import { messages } from '../models/messageschema';
 export const ioServer = (server: http.Server) => {
 	const io = new Server(server);
 	io.on('connection', async (socket) => {
-		console.log('Client Connected');
+		console.log('Client Connected OK');
 
 		try {
 			socket.on('addProduct', async (product) => {
