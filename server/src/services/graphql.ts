7a3086b1b61ddb29f4fa411026bf2888c68c4b97
joxpulp <joxpulp@gmail.com>
2021-12-09 17:00:14 -0500

    feat: add updateProduct and deleteProduct to graphql

--- server/src/services/graphql.ts
+++ server/src/services/graphql.ts
@@ -2,22 +2,31 @@ import { buildSchema } from 'graphql';
 import { Args } from '../models/interfaces';
 import { products } from '../models/productschema';
 
-export const graphqlSchema = buildSchema(`
-    type Query {
-        getProducts: [Product]
-        getProduct(id: String!): Product
-    }
+const gql = String.raw;
 
-    type Mutation {
-        addProduct(title: String!, price: Int!, thumbnail: String!): Product
-    },
+export const graphqlSchema = buildSchema(gql`
+	type Query {
+		getProducts: [Product]
+		getProduct(id: String!): Product
+	}
 
-    type Product {
-        _id: String
-        title: String
-        price: Int
-        thumbnail: String
-    }
+	type Mutation {
+		addProduct(title: String!, price: Int!, thumbnail: String!): Product
+		updateProduct(
+			id: String!
+			title: String
+			price: Int
+			thumbnail: String
+		): Product
+		deleteProduct(id: String!): Product
+	}
+
+	type Product {
+		_id: String
+		title: String
+		price: Int
+		thumbnail: String
+	}
 `);
 
 export const graphqlRoot = {
@@ -33,7 +42,17 @@ export const graphqlRoot = {
 			price: args.price,
 			thumbnail: args.thumbnail,
 		});
-        
+
 		return await product.save();
+	},
+	updateProduct: async (args: Args) => {
+		return await products.findByIdAndUpdate(
+			args.id,
+			{ $set: args },
+			{ runValidators: true, new: true }
+		);
+	},
+	deleteProduct: async (args: Args) => {
+		return await products.findByIdAndDelete(args.id);
 	},
 };
